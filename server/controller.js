37940f9d1739488dba8e211deb64beb3d808832e
Tomas Fisher <dontomassino777@gmail.com>
2022-01-19 13:28:23 -0700

    testing endpoints-db connection

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,29 @@
+// TODO: connect to the db
+require("dotenv").config();
+const {CONNECTION_STRING} = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
 module.exports = {
-    functionGoesHere: (req, res) => {
-        //function body here
+    getUserPosts: (req, res) => {
+        sequelize.query(`SELECT * FROM user_posts`)
+            .then((dbResults) => res.status(200).send(dbResults[0]))
+            .catch((err) => console.log(err));
+    },
+    newUserPost: (req, res) => {
+        const {userId, imgURL, date, caption} = req.body
+
+        sequelize.query(`INSERT INTO user_posts (user_info_id, post_image, post_date, post_caption)
+            VALUES (${userId}, '${imgURL}', '${date}', '${caption}');`)
+            .then((dbResult) => res.status(200).send(dbResult[0]))
+            .catch((err) => console.log(err));
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
 
 app.use(cors());
-app.use(express.json());  // When we want to be able to accept JSON.
+app.use(express.json());
 
-// const {getFunction, deleteFunction, createFunction, updateFunction} = require(“./controller.js”)
+const {getUserPosts, newUserPost} = require("./controller.js")
+
+// what is the correct endpoint URL needed here?
+app.get("/posts", getUserPosts);
+
+app.post("/posts", newUserPost);
 
 // app.get(“/endpoint”, getFunction);
 // app.delete(“/endpoint/:id”, deleteFunction);
 // app.post(“/endpoint”, createFunction);
 // app.put(“/endpoint/:id”, updateFunction);
 
-app.listen(4040, () => console.log('Server running on 4040'))
+app.listen(port, () => console.log(`Jammin on ${port}`));
