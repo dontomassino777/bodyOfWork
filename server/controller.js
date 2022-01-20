b185dcc20834cefc6aaa895ab89ecf683ee74100
Tomas Fisher <dontomassino777@gmail.com>
2022-01-20 09:27:08 -0700

    user auth started

--- server/controller.js
+++ server/controller.js
@@ -18,12 +18,30 @@ module.exports = {
             .then((dbResults) => res.status(200).send(dbResults[0]))
             .catch((err) => console.log(err));
     },
+    newUser: (req, res) => {
+        const {userName, firstName, lastName, emailAddress, userPw, date} = req.body
+
+        sequelize.query(`INSERT INTO user_info (user_name, first_name, last_name, email_address, user_pw, signup_date)
+            VALUES ('${userName}', '${firstName}', '${lastName}', '${emailAddress}', '${userPw}', '${date}');`)
+            .then((dbResult) => res.status(200).send(dbResult))
+            .catch((err) => console.log(err));
+    },
     newUserPost: (req, res) => {
         const {userId, imgURL, date, caption} = req.body
 
         sequelize.query(`INSERT INTO user_posts (user_info_id, post_image, post_date, post_caption)
-            VALUES (${userId}, '${imgURL}', '${date}', '${caption}');`)
+            VALUES (${userId}, '${imgURL}', '${date}', '${caption}')
+            RETURNING *;`)
             .then((dbResult) => res.status(200).send(dbResult[0]))
             .catch((err) => console.log(err));
+    },
+    putProfileImage: (req, res) => {
+        const {userName, imgURL} = req.body
+
+        sequelize.query(`INSERT INTO user_info (profile_image)
+        VALUES ('${imgURL}')
+        WHERE user_name = '${userName}';`)
+        .then((dbResult) => res.status(200).send(dbResult[0]))
+        .catch((err) => console.log(err));
     }
 }
--- server/index.js
+++ server/index.js
@@ -8,16 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const {getUserPosts, newUserPost} = require("./controller.js")
+const {getUserPosts, newUser, newUserPost, putProfileImage} = require("./controller.js")
 
-// what is the correct endpoint URL needed here?
+
 app.get("/posts", getUserPosts);
 
+app.post("/users", newUser);
 app.post("/posts", newUserPost);
 
-// app.get(“/endpoint”, getFunction);
-// app.delete(“/endpoint/:id”, deleteFunction);
-// app.post(“/endpoint”, createFunction);
-// app.put(“/endpoint/:id”, updateFunction);
+// how do I set up endpoint for a specific user's profile?
+app.put("/users/:id", putProfileImage);
+
 
 app.listen(port, () => console.log(`Jammin on ${port}`));
